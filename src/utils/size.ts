d23477d0e851373830c656f4e0f3000db9e5c01e
FLzyy <flzyy72@gmail.com>
2023-01-16 16:10:53 +0800

    minified file size

--- src/utils/size.ts
+++ src/utils/size.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { readdir, stat } from "fs/promises";
 import { join } from "path";
+import { minifySync } from "@swc/core";
+import { readFileSync, writeFileSync } from "fs";
 
 export const dirSize = async (
   dir: string,
-  exclude?: string[],
+  exclude?: string[]
 ): Promise<number> => {
   const files = await readdir(dir, { withFileTypes: true });
 
@@ -26,12 +28,25 @@ export const dirSize = async (
     return 0;
   });
 
-  return (await Promise.all(paths)).flat(Infinity).reduce(
-    (i, size) => i + size,
-    0,
-  );
+  return (await Promise.all(paths))
+    .flat(Infinity)
+    .reduce((i, size) => i + size, 0);
 };
 
 export const fileSize = async (file: string): Promise<number> => {
   return (await stat(file)).size;
+};
+
+export const minifiedSized = async (file: string): Promise<number> => {
+  writeFileSync(
+    `./${file}min`,
+    minifySync(readFileSync(file, "utf-8"), {
+      module: true,
+      ecma: 2017,
+      compress: true,
+      mangle: true,
+    }).code
+  );
+
+  return await fileSize(`${file}min`);
 };
